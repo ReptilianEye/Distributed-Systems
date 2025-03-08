595b0116644f3c40ad059922c3db36b0b5d24035
reptilianeye <piotr.rzadkowski@swmansion.com>
2025-03-08 09:13:40 +0100

    udp works

--- lab1_gniazda/chat/client/client.go
+++ lab1_gniazda/chat/client/client.go
@@ -7,24 +7,45 @@ import (
 	"math/rand"
 	"net"
 	"os"
+	"strings"
 )
 
-const buffSize = 4096
+const buffSize = 20000
+const addr = "127.0.0.1"
+const multicastAddr = "224.0.0.1:12345"
 
 func main() {
 	// Connect to the server
-	conn, err := net.Dial("tcp", "localhost:8080")
+	tcpConn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
+	var tcpPort int
+	_, err = fmt.Sscanf(tcpConn.LocalAddr().String(), "[::1]:%d", &tcpPort)
+	if err != nil {
+		fmt.Println("Failed to extract port from remote address:", err)
+		tcpConn.Close()
+		return
+	}
+	defer tcpConn.Close()
 
-	go handleIncomingMessages(conn)
+	udpConn, err := net.DialUDP(
+		"udp",
+		&net.UDPAddr{Port: tcpPort + 1, IP: net.ParseIP(addr)},
+		&net.UDPAddr{Port: 8080, IP: net.ParseIP(addr)},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer udpConn.Close()
+
+	go handleIncomingMessagesTCP(tcpConn)
+	go handleIncomingMessagesUDP(tcpPort)
 	// Authenticate
 	nickname := fmt.Sprintf("client%d", rand.Intn(1000))
 	fmt.Printf("Your nickname is: %s\n", nickname)
-	_, err = conn.Write([]byte(nickname))
+	_, err = tcpConn.Write([]byte(nickname))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -37,19 +58,25 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		// Send some data to the server
-		_, err = conn.Write(message)
+		switch {
+		case strings.HasPrefix(string(message), "U "):
+			_, err = udpConn.Write(message)
+		default:
+			_, err = tcpConn.Write(message)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		// Close the connection
 	}
 }
-func handleIncomingMessages(conn net.Conn) {
+func handleIncomingMessagesTCP(conn net.Conn) {
 	for {
-		// Read incoming data
 		message := make([]byte, buffSize)
 		_, err := conn.Read(message)
 		if err != nil {
@@ -57,7 +84,31 @@ func handleIncomingMessages(conn net.Conn) {
 			log.Fatal("Server closed the connection")
 			return
 		}
+		fmt.Println("(TCP)", string(message))
+	}
+}
+
+func handleIncomingMessagesUDP(port int) {
+	addr := net.UDPAddr{
+		Port: port,
+		IP:   net.ParseIP(addr),
+	}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	fmt.Println("Listening for UDP messages on port", port)
+	for {
+		// Read incoming data
+		message := make([]byte, buffSize)
+		_, _, err := conn.ReadFromUDP(message)
+		if err != nil {
+			fmt.Println(err)
+			log.Fatal("Server closed the connection")
+			return
+		}
 		// Print the incoming data
-		fmt.Printf("%s\n", message)
+		fmt.Println("(UDP)", string(message))
 	}
 }
--- lab1_gniazda/chat/server/server.go
+++ lab1_gniazda/chat/server/server.go
@@ -2,49 +2,85 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 const buffSize = 4096
+const port = 8080
+const addr = "127.0.0.1"
+
+type conns struct {
+	tcp net.Conn
+	udp *net.UDPConn
+}
 
 func main() {
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		panic(err)
 	}
 	defer ln.Close()
 
-	var clients = make(map[string]net.Conn)
+	udpAddr := net.UDPAddr{
+		Port: port,
+		IP:   net.ParseIP(addr),
+	}
+
+	var clients = make(map[string]conns)
+	conn, err := net.ListenUDP("udp", &udpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
 	// Accept incoming connections and handle them
 	for {
-		conn, err := ln.Accept()
+		tcpConn, err := ln.Accept()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 		if len(clients) >= 10 {
 			fmt.Println("Too many clients")
-			conn.Close()
+			tcpConn.Close()
 			continue
 		}
+		remoteAddr := tcpConn.RemoteAddr().String()
+		var remotePort int
+		_, err = fmt.Sscanf(remoteAddr, "[::1]:%d", &remotePort)
+		if err != nil {
+			fmt.Println("Failed to extract port from remote address:", err)
+			tcpConn.Close()
+			continue
+		}
+		udpConn, err := net.DialUDP("udp", nil, &net.UDPAddr{
+			Port: remotePort,
+			IP:   net.ParseIP(addr),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer udpConn.Close()
 
 		// Authenticate client
-		nicknameByte := make([]byte, 1024)
-		_, err = conn.Read(nicknameByte)
+		nicknameByte := make([]byte, buffSize)
+		_, err = tcpConn.Read(nicknameByte)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 		nickname := string(nicknameByte)
-		clients[nickname] = conn
+		clients[nickname] = conns{tcpConn, udpConn}
 		fmt.Printf("New client: %s\n", nickname)
 
 		// Handle the connection in a new goroutine
-		go handleConnection(conn, nickname, clients)
+		go handleUDPConnection(conn, nickname, clients)
+		go handleTCPConnection(tcpConn, nickname, clients)
 	}
 }
 
-func handleConnection(conn net.Conn, clientNick string, otherClients map[string]net.Conn) {
+func handleTCPConnection(conn net.Conn, clientNick string, otherClients map[string]conns) {
 	// Close the connection when we're done
 	defer conn.Close()
 	for {
@@ -56,18 +92,43 @@ func handleConnection(conn net.Conn, clientNick string, otherClients map[string]
 			delete(otherClients, clientNick)
 			return
 		}
-		sendToAllOther(clientNick, message, otherClients)
+		sendToAllOthersTCP(clientNick, message, otherClients)
 	}
 }
 
-func sendToAllOther(nickname string, message []byte, otherClients map[string]net.Conn) {
+func handleUDPConnection(conn *net.UDPConn, clientNick string, otherClients map[string]conns) {
+	for {
+		message := make([]byte, buffSize)
+		_, addr, err := conn.ReadFromUDP(message)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Received message from UDP %s: %s\n", addr, message)
+		sendToAllOthersUDP(clientNick, message, otherClients)
+	}
+}
+func sendToAllOthersTCP(nickname string, message []byte, otherClients map[string]conns) {
 	for nick, conn := range otherClients {
 		if nick != nickname {
 			mess := []byte(nickname + ": " + string(message))
-			_, err := conn.Write(mess)
+			_, err := conn.tcp.Write(mess)
 			if err != nil {
 				fmt.Println(err)
 			}
+		}
+	}
+}
+
+func sendToAllOthersUDP(nickname string, message []byte, otherClients map[string]conns) {
+	for nick, conn := range otherClients {
+		if nick != nickname {
+			mess := []byte(nickname + ": " + string(message))
+			_, err := conn.udp.Write(mess)
+			if err != nil {
+				fmt.Println(err)
+			}
+
 		}
 	}
 }
